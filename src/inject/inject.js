d98ee2f93e0dec6f392fda4866f577021945d3f9
TheBlueJ <jwrightbluj@gmail.com>
2018-08-02 13:40:11 -0700

    Added submit button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,15 +3,28 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+		let port = chrome.extension.connect({"What??":"How Does this wok?"});
+
+		let url = 'jofNR_WkoCE';
+
+		port.onMessage.addListener(function(msg) {
+			if([Object.keys(msg)[0] == "newURL"]){
+				console.log('testing');
+			}
+		  });
+
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		 console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
-		let video = $("<div id='videoPlayer'><iframe style='padding-top: 15px;' width='640' height='360'src='https://www.youtube.com/embed/jI-kpVh6e1U?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>");
+		let script = $("<script>window.onYouTubeIframeAPIReady = () => { console.log('RUNNING!!!!!!!!!'); player = new YT.Player('player', {height: '390',width: '640',videoId: 'M7lc1UVf-VE',events: {'onReady': onPlayerReady,'onStateChange': onPlayerStateChange}});} </script>")
+		$('body').append(script);
+
+		let video = $(`<div id='videoPlayer'><iframe style="padding-top: 15px;" width='640' height='360'src='https://www.youtube.com/embed/${url}?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>`);
 		video.css({'position' : 'fixed', 'bottom' : '0', 'right' : '0', 'z-index' : '1000000'});
+		video.draggable();
 		$('body').append(video);
-		$("#videoPlayer").draggable();
 
 		var tag = $(``);
 		$('body').append(tag);
