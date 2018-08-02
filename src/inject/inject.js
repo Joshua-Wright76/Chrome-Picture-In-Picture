98d0fe0d80dc7d833a453fb377e8c74c7c472ec7
G0ldenSp00n <hayfithyan@gmail.com>
2018-08-02 14:12:21 -0700

    Finished All Code to React to Resquests from Extension in Injection Script

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,32 +1,47 @@
+let loaded = false;
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
+    if (request.greeting[0] == "-"){
+			if(request.greeting[0] == "-topLeft"){
+				updateCorner("0", "auto", "0", "auto");
+			} else if (request.greeting[0] == "-topRight"){
+				updateCorner("0", "auto", "auto", "0");
+			} else if (request.greeting[0] == "-botLeft"){
+				updateCorner("auto", "0", "0", "auto");
+			} else if (request.greeting[0] == "-botRight"){
+				updateCorner("auto", "0", "auto", "0");
+			}
+		} else {
+			loadVideoPlayer(request.greeting[0]);
+		}
   });
 
-var readyStateCheckInterval = setInterval(function() {
-if (document.readyState === "complete") {
-	clearInterval(readyStateCheckInterval);
+function loadVideoPlayer(videoCode){
+	if (document.readyState === "complete") {
+		loaded = true;
+		clearInterval(readyStateCheckInterval);
 
-	// ----------------------------------------------------------
-	// This part of the script triggers when page is done loading
-	 console.log("Hello. This message was sent from scripts/inject.js");
-	// ----------------------------------------------------------
+		let video = $(`<div id='videoPlayer'><iframe style='padding-top: 15px;' width='640' height='360'src='https://www.youtube.com/embed/${videoCode}?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>`);
+		video.css({'position' : 'fixed', 'bottom' : '0', 'right' : '0', 'z-index' : '1000000'});
+		$('body').append(video);
+		$("#videoPlayer").draggable();
 
-	let video = $("<div id='videoPlayer'><iframe style='padding-top: 15px;' width='640' height='360'src='https://www.youtube.com/embed/jI-kpVh6e1U?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>");
-	video.css({'position' : 'fixed', 'bottom' : '0', 'right' : '0', 'z-index' : '1000000'});
-	$('body').append(video);
-	$("#videoPlayer").draggable();
+		// 4. The API will call this function when the video player is ready.
+		function onPlayerReady(event) {
+			event.target.playVideo();
+		}
 
-	// 4. The API will call this function when the video player is ready.
-	function onPlayerReady(event) {
-		event.target.playVideo();
+		// 3. This function creates an <iframe> (and YouTube player)
+		//    after the API code downloads.
 	}
-
-	// 3. This function creates an <iframe> (and YouTube player)
-	//    after the API code downloads.
 }
-}, 10);
+
+function updateCorner(top, bot, left, right) {
+	if(loaded) {
+		$("#videoPlayer").css({'position' : `${top} ${bot} ${left} ${right}`})
+	}
+}
