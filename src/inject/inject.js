7137143fb384638d05b14e68ce134c021608160a
TheBlueJ <jwrightbluj@gmail.com>
2018-08-02 14:06:29 -0700

    dded event listeners for coner buttons

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,30 +7,6 @@ chrome.runtime.onMessage.addListener(
       sendResponse({farewell: "goodbye"});
   });
 
-<<<<<<< HEAD
-		let port = chrome.extension.connect({"What??":"How Does this wok?"});
-
-		let url = 'jofNR_WkoCE';
-
-		port.onMessage.addListener(function(msg) {
-			if([Object.keys(msg)[0] == "newURL"]){
-				console.log('testing');
-			}
-		  });
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		 console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-		let script = $("<script>window.onYouTubeIframeAPIReady = () => { console.log('RUNNING!!!!!!!!!'); player = new YT.Player('player', {height: '390',width: '640',videoId: 'M7lc1UVf-VE',events: {'onReady': onPlayerReady,'onStateChange': onPlayerStateChange}});} </script>")
-		$('body').append(script);
-
-		let video = $(`<div id='videoPlayer'><iframe style="padding-top: 15px;" width='640' height='360'src='https://www.youtube.com/embed/${url}?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>`);
-		video.css({'position' : 'fixed', 'bottom' : '0', 'right' : '0', 'z-index' : '1000000'});
-		video.draggable();
-		$('body').append(video);
-=======
 var readyStateCheckInterval = setInterval(function() {
 if (document.readyState === "complete") {
 	clearInterval(readyStateCheckInterval);
@@ -39,7 +15,6 @@ if (document.readyState === "complete") {
 	// This part of the script triggers when page is done loading
 	 console.log("Hello. This message was sent from scripts/inject.js");
 	// ----------------------------------------------------------
->>>>>>> 9484e53532edbb0d976d8014c1c241425a455fde
 
 	let video = $("<div id='videoPlayer'><iframe style='padding-top: 15px;' width='640' height='360'src='https://www.youtube.com/embed/jI-kpVh6e1U?rel=0&autoplay=true' frameborder='0' allowfullscreen></iframe></div>");
 	video.css({'position' : 'fixed', 'bottom' : '0', 'right' : '0', 'z-index' : '1000000'});
